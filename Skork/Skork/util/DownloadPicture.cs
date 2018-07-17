9ecc9cd6e930781290225b706c5b2523927ed8e3
REAPIX\Reapism <reapsprgm@gmail.com>
2018-07-17 19:05:53 -0400

    Changes with downloading images. Handled locally.

--- Skork/Skork/util/DownloadPicture.cs
+++ Skork/Skork/util/DownloadPicture.cs
@@ -1,39 +1,17 @@
-﻿using System;
-using System.Drawing;
-using System.Drawing.Imaging;
+﻿using System.Drawing;
 using System.IO;
-using System.Net;
 using System.Windows.Forms;
+using System.Reflection;
+using System.Resources;
 
 namespace Skork.util {
-    class DownloadPicture {
+    class DownloadPicture {           
 
-        /// <summary>
-        /// Downloads an image from an unknown fileformat into a png.
-        /// </summary>
-        /// <param name="url">The url of the picture to download.</param>
-        /// <returns></returns>
+        public string getPath(string fileName) {
 
-        public Image downloadImage(string url) {
-            WebClient web = new WebClient();
-            byte[] data;
-            MemoryStream ms;
-            Image img;
-            try {
-                data = web.DownloadData(new Uri(url));
-                ms = new MemoryStream(data);
-                img = Image.FromStream(ms); 
-                img.Save(ms, ImageFormat.Png);
-                ms.Close();
-                return img;
-            } catch (Exception e) {
-                MessageBox.Show(e.ToString());
-                return null;
-            }
-
-            
+            return "";
         }
-        
+
         public Image downloadImage(string url, PictureBox p) {
             return null;
         }
