589109d1730a2114f5bff1d9aeeadcaf45cfff95
REAPIX\Reapism <reapsprgm@gmail.com>
2018-07-17 19:05:17 -0400

    General exception for Skork which contains a constructor for creating a message.

---
+++ Skork/Skork/util/SkorkException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skork.util {
+    class SkorkException : Exception {
+
+        private string exceptionMessage;
+
+        public SkorkException(string exceptionMessage) {
+            this.exceptionMessage = exceptionMessage;
+        }
+
+        public override string ToString() {
+            return this.exceptionMessage;
+        }
+
+    }
+}
