4579a64e81eda4bb87a240d30dc6046ee83918c7
REAPIX\Reapism <reapsprgm@gmail.com>
2018-08-02 20:32:19 -0400

    +SkorkInstructions - Stores the objects created within the Skork code.

--- Skork/Skork/util/SkorkInstructions.cs
+++ Skork/Skork/util/SkorkInstructions.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using Skork.keywords;
-using System;
 using System.Windows.Forms;
+using System.Collections;
+using Skork.util;
 
 namespace Skork.util {
 
     /// <summary>
-    /// Represents the instruction set for skork to follow.
+    /// Stores the instruction-set for skork to follow.
     /// </summary>
 
     class SkorkInstructions {
@@ -15,6 +16,7 @@ namespace Skork.util {
         private static Dictionary<string, double> doubles;
         private static Dictionary<string, bool> bools;
         private static Dictionary<string, SkorkSprite> sprites;
+        private SkorkConventions sc;
 
 
         static SkorkInstructions() {
@@ -24,41 +26,109 @@ namespace Skork.util {
             sprites = new Dictionary<string, SkorkSprite>();
         }
 
-        public bool containsKey(string name, Dictionary<string, int> dictionary) {
+        public SkorkInstructions() {
+            sc = new SkorkConventions();
+        }
+
+        private bool containsKey(string name, Dictionary<string, int> dictionary) {
             return (dictionary.ContainsKey(name));
         }
 
-        public bool containsKey(string name, Dictionary<string, double> dictionary) {
+        private bool containsKey(string name, Dictionary<string, double> dictionary) {
             return (dictionary.ContainsKey(name));
         }
 
-        public bool containsKey(string name, Dictionary<string, bool> dictionary) {
+        private bool containsKey(string name, Dictionary<string, bool> dictionary) {
             return (dictionary.ContainsKey(name));
         }
 
-        public bool containsKey(string name, Dictionary<string, SkorkSprite> dictionary) {
+        private bool containsKey(string name, Dictionary<string, SkorkSprite> dictionary) {
             return (dictionary.ContainsKey(name));
         }
 
-        public bool createInt(string name, int value) {
-            if (!containsKey(name, ints)) {
-                ints.Add(new SkorkKeys().ToString() + "\\" + name, value);
-                return true;        
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">Key: The name of the identifier.</param>
+        /// <param name="value">Value: The value of the identifier.</param>
+        /// <returns>If successful.</returns>
+
+        public bool createKey(string name, int value) {
+            
+            int ident = sc.isValidIdentifier(name);
+            SkorkConsole c = new SkorkConsole();
+
+            switch (ident) {
+                case 0:
+                    if (containsKey(name, ints)) {
+                        return false;
+                    } else {
+                        ints.Add(new SkorkKeysID().ToString() + "\\" + name, value);
+                        return true;
+                    }  
+                case 1:                    
+                    MessageBox.Show(
+                        name + " first character \"" + name[0] + "\" is invalid.",
+                        "SkorkInvalidIdentifierException");
+                    break;
+                case 2:
+                    MessageBox.Show(
+                        name + " contains a character that's invalid!",
+                        "SkorkInvalidIdentifierException");
+                    break;
+                case 3:
+                    MessageBox.Show(
+                        name + " is too long! Should be <= 25. It's " +
+                        name.Length + ".",
+                        "SkorkInvalidIdentifierException");
+                    break;
+                case 4:
+
+                    MessageBox.Show(
+                       name + " is a keyword! No variable can be a keyword.",
+                       "SkorkInvalidIdentifierException");
+                    break;
+
             }
             return false;
+                                 
         }
-
-        public void getList() {
-            MessageBox.Show("getList");
-            foreach(string name in ints.Keys) {
-                MessageBox.Show(name);
+        
+        public bool createKey(string name, double value) {
+            if (containsKey(name, doubles)) {
+                return false;
+            } else {
+                doubles.Add(name, value);
+                return true;
             }
         }
 
-        public bool hashInt() {
-            return false;
+        public bool createKey(string name, bool value) {
+            if (containsKey(name, bools)) {
+                return false;
+            } else {
+                bools.Add(name, value);
+                return true;
+            }
         }
 
+        public bool createKey(string name, SkorkSprite value) {
+            if (containsKey(name, sprites)) {
+                return false;
+            } else {
+                sprites.Add(name, value);
+                return true;
+            }
+        }
+
+        public void temp() {
+            foreach(KeyValuePair<string, int> entry in ints) {
+                MessageBox.Show($"{entry.Key} = {entry.Value}");
+            }
+
+
+        }
 
     }
 }
