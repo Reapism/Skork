c98f0e8cddced15c1dc33cbbd9fedf8e4c68887f
REAPIX\Reapism <reapsprgm@gmail.com>
2018-08-02 20:30:53 -0400

    -OutlineBox class + Conviently made it work off a constructor instead of a member function. Creating an object of OutlineBox and making it outline a box can all be done in 1 line of code instead of two.

--- Skork/Skork/util/OutlineBox.cs
+++ Skork/Skork/util/OutlineBox.cs
@@ -7,57 +7,47 @@ namespace Skork.util {
 
     /// <summary>
     /// OutlineBox is a class that takes a picturebox
-    /// and flicker it a certain color n times.
-    /// <para>- Call member function outlineControl(PictureBox,
-    /// byte = [], byte = []) to invoke the process.</para>
+    /// and flicker it a particular color num times.
+    /// <para>- Construct this object with the following 
+    /// parameters(PictureBox, Nullable&lt;Color&gt;,
+    /// byte = []) to invoke the process.</para>
     /// <para>+ Execution runs on a separate thread.</para>
     /// </summary>
 
-   class OutlineBox {
-        
+    class OutlineBox {
+
         private byte outlineNumTimes;
         private Color outlineColor;
         private Color outlineColor2;
         private Timer outlineTimer;
         private PictureBox p;
-        private byte type;
         private short interval;
-        
-        /// <summary>
-        /// Default constructor for OutlineBox.
-        /// </summary>
 
-        public OutlineBox() {
-            this.outlineNumTimes = 0;
+        /// <summary>
+        /// Creating a colored outline using a picturebox.
+        /// </summary>
+        /// <param name="p">The picturebox to perform the flicker</param>
+        /// <param name="type">Nullable type. The color to flicker or null for a random color.</param>
+        /// <param name="num">The number of times to flicker the picturebox.</param>
+
+        public OutlineBox(ref PictureBox p, Color? type, byte num = 3) {
+            this.outlineNumTimes = num;
             this.outlineColor = Color.White;
             this.outlineColor2 = Color.Transparent;
             this.outlineTimer = new Timer();
-            this.p = new PictureBox();
-            this.type = 0;
+            this.p = new PictureBox();           
             this.interval = 100;
-        }
 
-        /// <summary>
-        /// Destructor for instance variables.
-        /// </summary>
+            if (type.HasValue) {
+                this.outlineColor = (Color) type;
+            } else {
+                Random rnd = new Random();
+                outlineColor = Color.FromArgb(255, rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            }            
 
-        ~OutlineBox() {
-            
-        }
-
-        /// <summary>
-        /// Takes a picture box by reference, and outlines it num times.
-        /// Use type parameter to specify the type
-        /// </summary>
-        /// <param name="p">The picturebox to outline.</param>
-        /// <param name="type">The type of outline {1 = green, 2 = red, 3 = gold, type >= 4 = random}</param>
-        /// <param name="num">Number of times to flicker the color.</param>
-
-        public void outlineControl(ref PictureBox p, byte type, byte num = 3) {            
-            this.type = type; // copy parameter to member variable.
-            outlineTimer.Interval = this.interval; 
+            outlineTimer.Interval = this.interval;
             outlineTimer.Tick += outlineControl_Tick;
-            
+
             BackgroundWorker b = new BackgroundWorker();
             b.WorkerSupportsCancellation = true;
             b.WorkerReportsProgress = false;
@@ -69,26 +59,24 @@ namespace Skork.util {
             b.RunWorkerAsync(p); // gets passed into DoWork method as e.Argument
         }
 
+        /// <summary>
+        /// Takes a picture box by reference, and outlines it num times [Threaded].
+        /// <para>The type of outline {1 = green, 2 = red, 3 = gold, else = random color}</para>
+        /// </summary>
+        /// <param name="p">The picturebox to outline.</param>
+        /// <param name="type">The color to pass.</param>
+        /// <param name="num">Number of times to flicker the color.</param>
+
         private void outlineControl_DoWork(object sender, DoWorkEventArgs e) {
 
             if (e.Argument is PictureBox) {
-                this.p = (PictureBox) e.Argument;              
-                if (this.type == 1) {
-                    outlineColor = Color.Green;
-                } else if (this.type == 2) {
-                    outlineColor = Color.IndianRed;
-                } else if (this.type == 3) {
-                    outlineColor = Color.Gold;
-                } else {
-                    Random rnd = new Random();
-                    outlineColor = Color.FromArgb(255, rnd.Next(256), rnd.Next(256), rnd.Next(256));
-                }
+                this.p = (PictureBox)e.Argument;              
             } else {
                 throw new ArgumentException("Object passed is not a picturebox" + this.ToString());
             }
         }
 
-        private void outlineControl_Tick(object sender, EventArgs e) {            
+        private void outlineControl_Tick(object sender, EventArgs e) {
             if (outlineNumTimes <= 0) {
                 this.outlineTimer.Stop();
                 this.p.BackColor = this.outlineColor2;
@@ -102,9 +90,8 @@ namespace Skork.util {
                 } else {
                     return;
                 }
-                
             }
-
         }
+
     }
 }
