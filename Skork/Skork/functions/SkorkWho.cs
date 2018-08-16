e571b26e5e0c1f8169c521ca570dcf951e0a0354
REAPIX\Reapism <reapsprgm@gmail.com>
2018-08-16 02:57:54 -0400

    SkorkWho() Represents the who function which calls the passed objects ToString method.

---
+++ Skork/Skork/functions/SkorkWho.cs
@@ -0,0 +1,22 @@
+﻿namespace Skork.functions {
+
+    /// <summary>
+    /// Who(object) Function: 
+    /// <para>Calls the ToString() method on a specific object.</para>
+    /// Used for debugging your Skork code.
+    /// Usage new SkorkWho(object).ToString();
+    /// </summary>
+
+    class SkorkWho {
+
+        private string who;
+
+        public SkorkWho(object o) {
+            who = o.ToString();
+        }
+
+        public override string ToString() {
+            return who;
+        }
+    }
+}
