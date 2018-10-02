ad0d752523520aa8b13732b437e2373e1db30812
REAPIX\Reapism <reapsprgm@gmail.com>
2018-10-02 00:21:41 -0400

    Removed generic class SkorkKeywords and created less generic SkorkKeyword class to represent only keywords in skork and not types, and functions

---
+++ Skork/Skork/keywords/SkorkKeyword.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Specialized;
+
+namespace Skork.keywords {
+
+    /// <summary>
+    /// Stores a static reference to all the types, functions,
+    /// and generic keywords in Skork. Also contains member
+    /// functions for checking if a specific type, function, or
+    /// keyword is contained.
+    /// </summary>
+
+    class SkorkKeyword {
+        
+        public static readonly StringCollection KEYS;
+
+        static SkorkKeyword() {
+            KEYS = new StringCollection();
+            new SkorkKeyword().AddKeywords();
+        }      
+
+        private void AddKeywords() {
+            KEYS.Add("return");
+            KEYS.Add("if");
+            KEYS.Add("else");
+        }
+
+        public bool IsKeyword(string s) => (KEYS.Contains(s));
+
+        public StringCollection Keywords { get { return KEYS; } }
+
+    }
+}
